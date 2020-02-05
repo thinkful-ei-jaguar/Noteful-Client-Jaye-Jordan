4e77060b0f3a85a6d3593e5586cb79b28d733af1
Jordan Castillo <jordanxcallaway@gmail.com>
2020-02-05 07:20:07 -0800

    content and all data from database wired up to frontend logic

--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,9 +1,10 @@
+/* eslint-disable eqeqeq */
 
 export const findFolder = (folders=[], folderId) =>
   folders.find(folder => folder.id === folderId)
 
 export const findNote = (notes=[], noteId) =>
-  notes.find(note => note.id === noteId)
+  notes.find(note => note.id == noteId)
 
 export const getNotesForFolder = (notes=[], folderId) => (
   (!folderId)
